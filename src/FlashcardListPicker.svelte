150b2e81f0719f1fc20f2cb9faf0d93ce954b6b8
tntredstone <cgsteck@outlook.com>
2023-09-01 14:43:30 -0400

    made ability to add to flashcard lists

--- src/FlashcardListPicker.svelte
+++ src/FlashcardListPicker.svelte
@@ -1,13 +1,25 @@
 <script>
-	let flashcardLists = ["None selected currently"];
+    let flashcardLists = ["None selected currently"];
+    let newFlashcardListName = "";
+    let selectedList = "None selected currently";
+
+    function addFlashcardList() {
+        if (newFlashcardListName.trim() !== "") {
+            flashcardLists = [...flashcardLists, newFlashcardListName];
+            selectedList = newFlashcardListName; // Select the newly added option
+            newFlashcardListName = ""; // Clear the input field after adding
+        }
+    }
 </script>
 
 <main>
-	{#each flashcardLists as flashcardList}
-		<select>
-			<option value={flashcardList}>{flashcardList}</option>
-		</select>
-	{/each}
+    <select bind:value={selectedList}>
+        {#each flashcardLists as flashcardList}
+            <option value={flashcardList}>{flashcardList}</option>
+        {/each}
+    </select>
+    <input bind:value={newFlashcardListName} type="text" />
+    <button on:click={addFlashcardList}>Add</button>
 </main>
 
 <style></style>
