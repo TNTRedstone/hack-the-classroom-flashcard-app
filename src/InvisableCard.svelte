a62128949fd812786a340cad66c28c045f7d3687
tntredstone <cgsteck@outlook.com>
2023-09-01 11:59:43 -0400

    started on making flashcards

---
+++ src/InvisableCard.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let text;
+</script>
+
+<main>
+    <div class="invisablecard">
+        <h2>{text}</h2>
+    </div>
+</main>
+
+<style></style>
---
+++ src/VisableCard.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let text;
+</script>
+
+<main>
+    <div class="visablecard">
+        <h2>{text}</h2>
+    </div>
+</main>
+
+<style></style>
