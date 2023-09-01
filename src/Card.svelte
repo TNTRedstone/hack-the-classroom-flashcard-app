df648d49c910104b205f914ecd89d1fa52b01c5f
tntredstone <cgsteck@outlook.com>
2023-09-01 13:13:34 -0400

    improved card css

--- src/Card.svelte
+++ src/Card.svelte
@@ -1,11 +1,29 @@
-<script>
-    import VisableCard from "./VisableCard.svelte";
-    import InvisableCard from "./InvisableCard.svelte";
-</script>
+<script></script>
 
 <main>
-    <VisableCard></VisableCard>
-    <InvisableCard></InvisableCard>
+    <div class="invisablecard card">
+        <h2>side 2</h2>
+    </div>
+    <div class="visablecard card">
+        <h2>side 1</h2>
+    </div>
 </main>
 
-<style></style>
+<style>
+    main {
+        display: relative;
+    }
+
+    .card {
+        position: absolute;
+    }
+
+    .visablecard {
+        background-color: #ef2bb8;
+        filter: drop-shadow(2px 2px 0.5px #003a61);
+    }
+
+    .invisablecard {
+        background-color: #003a61;
+    }
+</style>
