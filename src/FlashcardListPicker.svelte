ade0f8ca8c6991588ca3ac7fd87bd13c4d5f92fd
tntredstone <cgsteck@outlook.com>
2023-09-02 08:31:14 -0400

    added ability to add cards

--- src/FlashcardListPicker.svelte
+++ src/FlashcardListPicker.svelte
@@ -1,26 +1,45 @@
 <script>
-    let flashcardLists = ["None selected currently"];
+    let flashcardLists = [{ name: "None selected currently", data: [] }];
     let newFlashcardListName = "";
-    let selectedList = "None selected currently";
+    let selectedListIndex = 0; // Initialize with the index of the default list
+    let newCardFront = "";
+    let newCardBack = "";
 
     function addFlashcardList() {
         if (newFlashcardListName.trim() !== "") {
-            flashcardLists = [...flashcardLists, newFlashcardListName];
-            selectedList = newFlashcardListName; // Select the newly added option
+            flashcardLists.push({ name: newFlashcardListName, data: [] });
+            selectedListIndex = flashcardLists.length - 1; // Select the newly added option
             newFlashcardListName = ""; // Clear the input field after adding
+        }
+    }
+
+    function addCard() {
+        if (newCardFront.trim() !== "" && newCardBack.trim() !== "") {
+            let newCard = {
+                front: newCardFront,
+                back: newCardBack,
+            };
+            flashcardLists[selectedListIndex].data.push(newCard);
+            newCardFront = "";
+            newCardBack = "";
         }
     }
 </script>
 
 <main>
-    <select bind:value={selectedList}>
-        {#each flashcardLists as flashcardList}
-            <option value={flashcardList}>{flashcardList}</option>
+    <select bind:value={selectedListIndex}>
+        {#each flashcardLists as flashcardList, index}
+            <option value={index}>{flashcardList.name}</option>
         {/each}
     </select>
     <div>
         <input bind:value={newFlashcardListName} type="text" />
         <button on:click={addFlashcardList}>Add</button>
+    </div>
+    <div>
+        <input type="text" placeholder="front of card" bind:value={newCardFront}>
+        <input type="text" placeholder="back of card" bind:value={newCardBack}>
+        <button on:click={addCard}>Add</button>
     </div>
 </main>
 
